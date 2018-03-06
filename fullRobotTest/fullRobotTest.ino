422109e007158dfbf25f6ab83fe39ebbd9dce6c3
richardlyf <richardlyf@users.noreply.github.com>
2018-03-05 17:53:16 -0800

    added fullrobot test
    the full robot test includes all helper funcitons and state machine. Use this to test the robot on the field

--- fullRobotTest/fullRobotTest.ino
+++ fullRobotTest/fullRobotTest.ino
@@ -1,4 +1,9 @@
 #include<AccelStepper.h>
+#include<Servo.h>
+#include <Metro.h>
+
+int servo = A9;
+Servo latch;
 
 #define STEP_HL 14
 #define DIR_HL 15
@@ -17,6 +22,15 @@ AccelStepper motor_horizontalR(1, STEP_HR, DIR_HR);
 AccelStepper motor_verticalL(1, STEP_VL, DIR_VL);
 AccelStepper motor_verticalR(1, STEP_VR, DIR_VR);
 
+static Metro timer = Metro(0);
+int state = 0;
+
+//predeclaration
+bool waitForTime(int delayTime);
+int mapSpeed(int speedUnmapped);
+void runMotor(char* motor, int speedinput, int dir);
+void moveLatch(char* pos);
+
 void setup() {
   Serial.begin(9600);
   motor_horizontalL.setMaxSpeed(MAX_SPEED);
@@ -32,6 +46,19 @@ void setup() {
   pinMode(STEP_VR, OUTPUT);
   pinMode(DIR_VL, OUTPUT);
   pinMode(DIR_VR, OUTPUT);
+
+  latch.attach(servo);
+  moveLatch("close");
+  delay(2000);
+}
+
+bool waitForTime(int delayTime){
+  timer.interval(delayTime);
+  if(timer.check()){
+    timer.reset();
+    return true;
+  }
+  else return false;
 }
 
 int mapSpeed(int speedUnmapped){
@@ -60,8 +87,40 @@ void runMotor(char* motor, int speedinput, int dir){
   }
 }
 
+void moveLatch(char* pos){
+  if(pos == "open"){
+    latch.write(180);
+  }
+  else if(pos == "close"){
+    latch.write(0);
+  }
+  else{
+    Serial.println("moveLatch input error!");
+  }
+}
+
 void loop() {
   static int dir = 1;
+
+  switch(state){
+    case 0:
+      runMotor("Horizontal", 100, dir);
+      if(waitForTime(2000)) state += 1;
+      break;
+
+    case 1:
+      runMotor("Horizontal", 0, dir);
+      runMotor("Vertical", 100, dir);
+      if(waitForTime(2000)) state = 0;
+      break;
+  }
+  /*
   runMotor("Horizontal", 100, dir);
   runMotor("Vertical", 100, dir);
+  moveLatch("close");
+
+  if(waitForTime(2000)){
+    Serial.println("done!");
+  }
+  */
 }
